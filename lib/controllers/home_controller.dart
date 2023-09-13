1e1d750d3a9658fc85137b6be18a8ae9746a42c7
meetmeerahul <meetmeerahul@gmail.com>
2023-09-13 17:19:13 +0530

    UI compleated with using GetX

---
+++ lib/controllers/home_controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  final slidervalue = 10.0.obs;
+
+  final upper = false.obs;
+  final lower = false.obs;
+  final numbers = false.obs;
+  final symbol = false.obs;
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:password_generator/controllers/home_controller.dart';
+import 'package:password_generator/screens/widgets/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final homeController = Get.put(HomeController());
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.save_as_outlined),
+          ),
+        ],
+        backgroundColor: const Color.fromARGB(255, 177, 169, 193),
+        title: const Text(
+          "Password Generator",
+          style:
+              TextStyle(fontWeight: FontWeight.bold, color: Colors.deepPurple),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            sizedBox(20),
+            TextField(
+              showCursor: false,
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+              ),
+            ),
+            sizedBox(20),
+            Obx(
+              () => Slider(
+                  value: homeController.slidervalue.value.toDouble(),
+                  max: 20,
+                  divisions: 5,
+                  label: homeController.slidervalue.value.toInt().toString(),
+                  onChanged: (value) {
+                    homeController.slidervalue.value = value;
+                  }),
+            ),
+            sizedBox(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: Obx(
+                    () => OutlinedButton(
+                      onPressed: () {
+                        homeController.upper.value =
+                            !homeController.upper.value;
+                        print(homeController.upper.value);
+                      },
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(width: 1.0),
+                          backgroundColor: homeController.upper.value
+                              ? Colors.teal.shade300
+                              : Colors.white),
+                      child: const Text('Uppercase'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Obx(
+                    () => OutlinedButton(
+                      onPressed: () {
+                        homeController.lower.value =
+                            !homeController.lower.value;
+                      },
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(width: 1.0),
+                          backgroundColor: homeController.lower.value
+                              ? Colors.teal.shade300
+                              : Colors.white),
+                      child: const Text('Lowercase'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            sizedBox(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Obx(
+                    () => OutlinedButton(
+                      onPressed: () {
+                        homeController.numbers.value =
+                            !homeController.numbers.value;
+                      },
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(width: 1.0),
+                          backgroundColor: homeController.numbers.value
+                              ? Colors.teal.shade300
+                              : Colors.white),
+                      child: const Text('Numbers'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Obx(
+                    () => OutlinedButton(
+                      onPressed: () {
+                        homeController.symbol.value =
+                            !homeController.symbol.value;
+                      },
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(width: 1.0),
+                          backgroundColor: homeController.symbol.value
+                              ? Colors.teal.shade300
+                              : Colors.white),
+                      child: const Text('Lowercase'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            sizedBox(20),
+            Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                        side: const BorderSide(width: 1.0),
+                        backgroundColor:
+                            const Color.fromARGB(255, 177, 169, 193)),
+                    child: const Text('Generate Password'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                        side: const BorderSide(width: 1.0),
+                        backgroundColor:
+                            const Color.fromARGB(255, 177, 169, 193)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [Icon(Icons.clear), Text('Clear')],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                        side: const BorderSide(width: 1.0),
+                        backgroundColor:
+                            const Color.fromARGB(255, 177, 169, 193)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [Icon(Icons.save), Text('Save')],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
